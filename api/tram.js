51a2f69c28067245eaf309d309686bbc756048f4
yoghi07 <yoghi07@gmail.com>
2025-03-26 13:33:17 +0100

    Update tram.js

--- api/tram.js
+++ api/tram.js
@@ -12,13 +12,18 @@ module.exports = async (req, res) => {
   try {
     const response = await fetch("https://giromilano.atm.it/proxy.tpportal/api/tpPortal/geodata/pois/5152541");
 
+    const text = await response.text();
+
     if (!response.ok) {
+      console.error("ATM responded with status:", response.status);
+      console.error("ATM response body:", text);
       return res.status(response.status).send("Errore ATM");
     }
 
-    const data = await response.json();
+    const data = JSON.parse(text);
     res.status(200).json(data);
   } catch (err) {
+    console.error("Fetch error:", err);
     res.status(500).send("Errore nel proxy: " + (err.message || err.toString()));
   }
 };
