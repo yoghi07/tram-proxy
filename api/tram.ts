38a79f26cbe7ea93a15c19f425ca64c1cceaea22
yoghi07 <yoghi07@gmail.com>
2025-03-26 13:15:46 +0100

    Create tram.ts

---
+++ api/tram.ts
@@ -0,0 +1,24 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  try {v
+    const response = await fetch("https://giromilano.atm.it/proxy.tpportal/api/tpPortal/geodata/pois/5152541");
+
+    if (!response.ok) {
+      return res.status(response.status).send("Errore ATM");
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err: any) {
+    res.status(500).send("Errore nel proxy: " + (err.message || err.toString()));
+  }
+}
